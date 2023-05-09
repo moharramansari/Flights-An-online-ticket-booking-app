020dcc9b4d98dc3e0678b33556285256dd0c489d
Moharram Ansari <moharramansari@gmail.com>
2023-05-09 17:27:27 +0530

    Initialize the setup and ErrorHandling, Validation and one CRUD functionalities

--- src/middlewares/airplane-middlewares.js
+++ src/middlewares/airplane-middlewares.js
@@ -1,13 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse } = require("../utils/common");
 
 function validateCreateRequest(req, res, next) {
   if (!req.body.modelNumber) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      success: false,
-      message: "Something went wrong while creating airplace",
-      data: {},
-      error: { explanation: "Model number not found in the uncomin" },
-    });
+    ErrorResponse.message = "Something went wrong while creating airplane";
+    ErrorResponse.error = new AppError(
+      ["Model Number not found in the oncoming request in the correct form"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
 }
