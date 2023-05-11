36cdf36ff95950281c5f5e8cd287366450e6c5eb
Moharram Ansari <moharramansari@gmail.com>
2023-05-11 08:18:34 +0530

    Refactoring crud-repositories

--- src/repositories/crud-repositories.js
+++ src/repositories/crud-repositories.js
@@ -6,51 +6,31 @@ class CrudRespositories {
   }
 
   async create(data) {
-    try {
-      const response = await this.model.create(data);
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the crud repo : create");
-      throw error;
-    }
+    const response = await this.model.create(data);
+    return response;
   }
 
   async destroy(data) {
-    try {
-      const response = await this.model.destroy({
-        where: {
-          id: data,
-        },
-      });
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the Crud Repo : destroy");
-      throw error;
-    }
+    const response = await this.model.destroy({
+      where: {
+        id: data,
+      },
+    });
+    return response;
   }
 
   async getAll() {
-    try {
-      const response = await this.model.findAll();
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the Crud Repo : get");
-      throw error;
-    }
+    const response = await this.model.findAll();
+    return response;
   }
 
   async update(id, data) {
-    try {
-      const response = await this.model.update(data, {
-        where: {
-          id: id,
-        },
-      });
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the Crud Repo : create");
-      throw error;
-    }
+    const response = await this.model.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    return response;
   }
 }
 
