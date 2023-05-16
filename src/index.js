d511177401e2cd966bcbf707eac144de1e307a99
Moharram Ansari <moharramansari@gmail.com>
2023-05-16 14:07:17 +0530

    Refactor main module

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
 });
